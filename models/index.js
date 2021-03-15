879bdfd02c68852a0819c5ea7ef15143a9c31518
Green S <kaylasierrabartley@gmail.com>
2021-03-15 09:49:46 -0500

    13.5

--- models/index.js
+++ models/index.js
@@ -40,5 +40,20 @@ User.hasMany(Vote, {
 Post.hasMany(Vote, {
     foreignKey: 'post_id'
 });
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+  
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
 
-module.exports = { User, Post, Vote };
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -16,6 +16,14 @@ router.get('/', (req, res) => {
     order: [['created_at', 'DESC']],
     include: [
       {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
         model: User,
         attributes: ['username']
       }
@@ -41,6 +49,14 @@ router.get('/:id', (req, res) => {
       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
     include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'created_at'],
+        include: {
+          model: Post,
+          attributes: ['title']
+        }
+      },
       {
         model: User,
         attributes: ['username']
