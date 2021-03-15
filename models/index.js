ebd3396d293d21af4c51862cb475bf604ed4e9d2
Green S <kaylasierrabartley@gmail.com>
2021-03-15 00:01:15 -0500

    13.4

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 // import all models
 const Post = require('./Post');
 const User = require('./User');
+const Vote = require('./Vote');
 
 // create associations
 User.hasMany(Post, {
@@ -10,5 +11,34 @@ User.hasMany(Post, {
 Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
+// for which posts a single user votes on
 
-module.exports = { User, Post };
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+// for which users voted on a single post
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+//which user voted
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+//what post is being voted on  
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+  
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+  
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote };
